3bc374742d2b5fc4fb5d5c83e3439f3769f144c3
ron2592 <ron2592@gmail.com>
2026-05-01 03:37:11 -0400

    fix supabase client throw during build

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-if (!supabaseUrl) throw new Error('supabaseUrl is required.')
-if (!supabaseAnonKey) throw new Error('supabaseAnonKey is required.')
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ""
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
